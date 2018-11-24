803015c1315fcbfda792b924f21ee11dfcca2574
phaux <phaux4@gmail.com>
2018-11-24 17:01:34 +0100

    babel

--- types.d.ts
+++
@@ -1,4 +0,0 @@
-declare module "string-replace-async" {
-  function stringReplaceAsync(s: string, r: RegExp, cb: (...args: string[]) => Promise<string>): Promise<string>
-  export = stringReplaceAsync
-}
---
+++ types/babel-plugin.d.ts
@@ -0,0 +1,4 @@
+declare module 'babel-plugin-*' {
+  const plugin: any
+  export = plugin
+}
